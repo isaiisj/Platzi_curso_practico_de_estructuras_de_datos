378d50fea965b4deb9d3bd698df3667fb0349ed5
Isai <47548422+isaiisj@users.noreply.github.com>
2024-11-22 14:20:54 -0600

    Create UserDefinedDataType.js

---
+++ 1.UserDefinedDataType/UserDefinedDataType.js
@@ -0,0 +1,13 @@
+class client 
+{
+    constructor (name, id, credit, adress){
+        this.name = name
+        this.id = id
+        this.credit = credit
+        this.adress = adress
+    }
+}
+
+let client1 = new client ("Camilo Valencia", 1, 1000000, "caller 1, carrera 1, ciudad Bolivar")
+
+console.log(client1);```
